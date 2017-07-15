e679cc8270126c762c591fee1885c805d9c67a80
Chetan Sanghani <crsanghani@gmail.com>
2017-07-15 22:25:33 +0100

    GifList container written

---
+++ src/components/GifList.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import GifItem from 'GifItem';
+
+// takes the gifs obj from index and maps the ids in to the component
+
+export default const GifList = (props) => {
+  const gifItems = props.gifs.map((image) => {
+    return <GifItem key=(image.id) gif={image} />
+    console.log(gifItems);
+  });
+
+// displays the variable that is created by the list container
+  return (
+    <ul>{gifItems}</ul>
+  );
+};
+
+// this is a functional stateless component
+
+// this means it doesnt track or modify the apps state, it is responsible for
+// for taking a list from app and rendering a GifItem obj for each item in the array
--- src/index.js
+++ src/index.js
@@ -1,8 +1,37 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import GifList from './components/GifList';
 import Searchbar from './components/Searchbar';
 
 class App extends React.Component {
+
+  constructor() {
+    super();
+    // dummy data
+    this.state = {
+      gifs: [
+        {
+          id: 1,
+          url: 'http://fakeimg.pl/300'
+        },
+        {
+          id: 2,
+          url: 'http://fakeimg.pl/300'
+        },
+        {
+          id: 3,
+          url: 'http://fakeimg.pl/300'
+        },
+        {
+          id: 4,
+          url: 'http://fakeimg.pl/300'
+        }
+      ]
+    }
+
+  }
+
+  // term change handles the input change from Searchbar state
   handleTermChange(term) {
     console.log(term);
   }
@@ -11,6 +40,7 @@ class App extends React.Component {
     return (
       <div>
         <Searchbar onTermChange={this.handleTermChange} />
+        <GifList gifs={this.state.gifs} />
       </div>
     );
   }

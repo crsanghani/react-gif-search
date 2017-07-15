c7cb3c9ecd421340fd432f2995bc643cc2c33af6
Chetan Sanghani <crsanghani@gmail.com>
2017-07-15 16:05:54 +0100

    searchbar component written and event handler with state modifier added.

---
+++ src/components/Searchbar.js
@@ -0,0 +1,23 @@
+import React from 'react';
+
+export default class Searchbar extends React.Component {
+
+  constructor() {
+    super();
+    this.state = { term: '' };
+  }
+
+  onInputChange(term) {
+    this.setState({ term });
+    this.props.onTermChange(term);
+  }
+
+  render() {
+    return (
+      <div className='search'>
+        <input onChange={e => this.onInputChange(e.target.value)} />
+      </div>
+    );
+  }
+
+};
--- src/index.js
+++ src/index.js
@@ -1,11 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import Searchbar from './components/Searchbar';
 
 class App extends React.Component {
+  handleTermChange(term) {
+    console.log(term);
+  }
+
   render() {
     return (
-      <div className='greeting'>
-        <p className='greeting-text'>Hello World!</p>
+      <div>
+        <Searchbar onTermChange={this.handleTermChange} />
       </div>
     );
   }

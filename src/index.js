06764d695804e9b5e8c785a7e51a9c1655d39da3
Chetan Sanghani <crsanghani@gmail.com>
2017-07-15 23:19:46 +0100

    refactored code to use giphy api and removed dummy date from app root

--- src/index.js
+++ src/index.js
@@ -2,44 +2,35 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import GifList from './components/GifList';
 import Searchbar from './components/Searchbar';
+import request from 'superagent';
 
 class App extends React.Component {
 
   constructor() {
     super();
-    // dummy data
+    // default state set within the constructor
     this.state = {
-      gifs: [
-        {
-          id: 1,
-          url: 'http://fakeimg.pl/300'
-        },
-        {
-          id: 2,
-          url: 'http://fakeimg.pl/300'
-        },
-        {
-          id: 3,
-          url: 'http://fakeimg.pl/300'
-        },
-        {
-          id: 4,
-          url: 'http://fakeimg.pl/300'
-        }
-      ]
+      gifs: []
     }
 
   }
 
   // term change handles the input change from Searchbar state
   handleTermChange(term) {
-    console.log(term);
+
+    // giphy api key react-gif-app
+    const url = `http://api.giphy.com/v1/gifs/search?q=${term.replace(/\s/g, '+')}&api_key=4b8da8802f574aa8bbb3571b5b7adab0`;
+
+    // superagent api handler alt to axios
+    request.get(url, (err, res) => {
+      this.setState({ gifs: res.body.data });
+    });
   }
 
   render() {
     return (
       <div>
-        <Searchbar onTermChange={this.handleTermChange} />
+        <Searchbar onTermChange={term => this.handleTermChange(term)} />
         <GifList gifs={this.state.gifs} />
       </div>
     );
